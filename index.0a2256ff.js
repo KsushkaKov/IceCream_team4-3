d13f45a2ff3e048269ef7b8972ee6cc7cd7a96fe
KsushkaKov <108684940+KsushkaKov@users.noreply.github.com>
2022-10-11 16:54:33 +0000

    Deploying to gh-pages from @ KsushkaKov/IceCream_team4-3@3c16905812ef560ea04de5e17602fcd740610b9f 🚀

---
+++ index.0a2256ff.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[data-modal-icecream-open]"),closeModalBtn:document.querySelector("[data-modal-icecream-close]"),modal:document.querySelector("[data-modal-icecream]")};function o(){e.modal.classList.toggle("backdrop-products--is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.0a2256ff.js.map
---
+++ index.86db8765.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-icecream-open]"),closeModalBtn:document.querySelector("[data-modal-icecream-close]"),modal:document.querySelector("[data-modal-icecream]")};function o(){e.modal.classList.toggle("backdrop-products--is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.86db8765.js.map
