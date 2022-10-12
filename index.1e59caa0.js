7e84202f22e9857134c826cb6a14321929265145
KsushkaKov <108684940+KsushkaKov@users.noreply.github.com>
2022-10-12 09:26:57 +0000

    Deploying to gh-pages from @ KsushkaKov/IceCream_team4-3@a6c91ba90279cfbbf68cb446be655a0f90a39505 🚀

--- index.1e59caa0.js
+++
@@ -1,2 +0,0 @@
-var swiper=new Swiper(".swiper-container",{direction:"horizontal",loop:!0,spaceBetween:30,pagination:{el:".swiper-pagination",clickable:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
-//# sourceMappingURL=index.1e59caa0.js.map
