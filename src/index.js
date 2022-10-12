f43b2aa2e94158529f5e6d6c64a59ba6b7e3f0b9
KsushkaKov <ksushka.ua@gmail.com>
2022-10-12 18:06:11 +0000

    validation proccess

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import '../js/mobile-menu.js';
+import '../js/modal-buy.js';
 import '../js/products-menu.js';
+import '../js/modal__about.js';
 import '../js/slider.js';
 import '../js/slider-galery.js';
