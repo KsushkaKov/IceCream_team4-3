8a737d966595a79200096441d3a3784ebd478b54
KsushkaKov <108684940+KsushkaKov@users.noreply.github.com>
2022-10-12 09:25:50 +0000

    Deploying to gh-pages from @ KsushkaKov/IceCream_team4-3@8d5ca275d885ba017b0e80e49c31510a918879c7 🚀

---
+++ index.850d3ce0.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-buy-open]"),closeModalBtn:document.querySelector("[data-modal-buy-close]"),modal:document.querySelector("[data-modal-buy]")};function o(){e.modal.classList.toggle("backdrop-hero--is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.850d3ce0.js.map
